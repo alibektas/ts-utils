45e57f9c443bbe4a65c41ad3869395b8e7694394
Ali Bektas <bektasali@protonmail.com>
2022-10-15 00:17:26 +0200

    Replace 'leave' with 'exit'

--- stateful/app/AppState.ts
+++ stateful/app/AppState.ts
@@ -1,33 +1,33 @@
 import type { IAppState } from "./IAppState";
 import hotkeys from "hotkeys-js";
 
-export abstract class State<StateName extends string, StateInput>
-  implements IAppState
+export abstract class AppState<StateName extends string, StateInput>
+implements IAppState
 {
-  constructor(protected state_name: StateName) {}
+	constructor(protected state_name: StateName) {}
 
-  protected _state: StateInput | null = null;
+	protected _state: StateInput | null = null;
 
-  get name(): StateName {
-    return this.state_name;
-  }
+	get name(): StateName {
+		return this.state_name;
+	}
 
-  onEnter(o: StateInput) {
-    this._state = o;
-    this.enter_hotkey_scope();
-  }
+	onEnter(o: StateInput) {
+		this._state = o;
+		this.enter_hotkey_scope();
+	}
 
-  onExit() {
-    this.leave_hotkey_scope();
-  }
+	onExit() {
+		this.exit_hotkey_scope();
+	}
 
-  enter_hotkey_scope() {
-    hotkeys.setScope(this.name);
-  }
+	enter_hotkey_scope() {
+		hotkeys.setScope(this.name);
+	}
 
-  leave_hotkey_scope() {
-    throw new Error("NotImplemented");
-  }
+	exit_hotkey_scope() {
+		throw new Error("NotImplemented");
+	}
 }
 
 export interface ABC {}
