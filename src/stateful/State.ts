3daa8c436551b52cee8343e42c952814dabe86a0
Ali Bektas <bektasali@protonmail.com>
2022-10-16 18:35:00 +0200

    Create state.ts ABC

---
+++ src/stateful/State.ts
@@ -0,0 +1,21 @@
+import { IState } from "./IState";
+
+export abstract class State<StateName extends string , StateInput> implements IState {
+
+    private _state : StateInput | null = null;
+
+    /**
+     *
+    */
+    constructor(protected state_name : StateName) {}
+    
+    onEnter(o : StateInput): void {
+        console.log(`utils-ts onEnter() , state : ${this.state_name}`);
+		this._state = o;
+    }
+
+    onExit(): void {
+		console.log(`utils-ts onExit() , state : ${this.state_name}`);
+    }
+
+}
