f03a06353f56a6fc79524e34a1dc8b6c553f77ab
Ali Bektas <bektasali@protonmail.com>
2022-10-17 01:50:15 +0200

    Bu kadar yeter

--- src/stateful/State.ts
+++ src/stateful/State.ts
@@ -29,3 +29,5 @@ export abstract class State<StateName extends string , StateInput> implements IS
     }
 
 }
+
+
--- src/stateful/StateMachine.d.ts
+++ src/stateful/StateMachine.d.ts
@@ -2,6 +2,6 @@ import type { IState } from "./IState";
 
 export interface IStateMachine<States extends string> {
 	states: { [name in States]: IState };
-	get state(): IState;
+	get state(): States;
 	transition(to: States): void;
 }
